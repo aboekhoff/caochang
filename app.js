b289ee8fc990d7d481d8c05c969a83d09e7564ff
Andrew Boekhoff <andrew.boekhoff@kaybus.com>
2014-12-22 12:00:36 -0800

    add jquery and drawing function select widget

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ function randByte() {
 }
 
 function randRGB() {
-  return 'rgb(' + [randByte(), randByte(), randByte()].join(',') + ')'
+  return 'rgb(' + [randByte(), randByte(), randByte()].join(',') + ')';
+}
+
+function resetCanvas() {
+  var canvas = document.getElementById('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - $('#select').height();
+}
+
+function drawCurrentSelection() {
+  drawingFunctions[$('#select').val()]();
 }
 
 function getContext() {
@@ -12,101 +22,102 @@ function getContext() {
   return ctx;
 }
 
-function simpleRectangle() {
+var drawingFunctions = {
+
+  simpleRectangle: function() {
   var ctx = getContext();
 
   //       x   y   w    h
   ctx.rect(20, 20, 100, 50);
   ctx.stroke();
-}
+  },
 
-function rainbowRectangles() {
-  var ctx = getContext();
+  rainbowRectangles: function() {
+    var ctx = getContext();
 
-  var dx = 2;
-  var dy = 2;
-  var dw = 2;
-  var dh = 2;
+    var dx = 2;
+    var dy = 2;
+    var dw = 2;
+    var dh = 2;
 
-  var x = 20;
-  var y = 20;
-  var w = 200;
-  var h = 100;
+    var x = 20;
+    var y = 20;
+    var w = 200;
+    var h = 100;
 
-  for (var i=0; i<30; i++) {
+    for (var i=0; i<30; i++) {
     
-    console.log(color);
+      // draw the shape
+      ctx.beginPath();
+      ctx.rect(x, y, w, h);
+      ctx.closePath();
 
-    // draw the shape
-    ctx.beginPath();
-    ctx.rect(x, y, w, h);
-    ctx.closePath();
-
-    // set the fill color and paint it
-    var color = randRGB();
-    ctx.strokeStyle = color;
-    ctx.stroke();  
+      // set the fill color and paint it
+      var color = randRGB();
+      ctx.strokeStyle = color;
+      ctx.stroke();  
     
-    // update the variables
-    x += dx;
-    y += dy;
-    w += dw;
-    h += dh;
+      // update the variables
+      x += dx;
+      y += dy;
+      w += dw;
+      h += dh;
+    }
+
+  },
+
+  rotatingRectangles: function() {
+    var ctx = getContext();
+
+    var dx = 6;
+    var dy = 6;
+    var dw = 2;
+    var dh = 2;
+    var dr = 4;
+
+    var x = 20;
+    var y = 20;
+    var w = 100;
+    var h = 20;
+    var r = 0;
+
+    for (var i=0; i<80; i++) {
+      var _w = Math.floor(w/2);
+      var _h = Math.floor(h/2);
+
+      ctx.save();
+
+      ctx.translate(x + _w, y + _h);
+      ctx.rotate(r * (Math.PI/180));
+
+      ctx.beginPath();
+      ctx.rect(-_w, -_h, w, h);
+      ctx.closePath();
+
+      ctx.stroke();
+      ctx.fillStyle = randRGB();
+      ctx.fill();
+      ctx.restore();
+
+      x += dx;
+      y += dy;
+      w += dw;
+      h += dh;
+      r += dr;
+
+    }
 
   }
 
-}
+};
 
-function rotatingRectangles() {
-  var ctx = getContext();
-
-  var dx = 6;
-  var dy = 6;
-  var dw = 2;
-  var dh = 2;
-  var dr = 4;
-
-  var x = 20;
-  var y = 20;
-  var w = 100;
-  var h = 20;
-  var r = 0;
-
-  for (var i=0; i<50; i++) {
-    var _w = Math.floor(w/2);
-    var _h = Math.floor(h/2);
-
-    ctx.save();
-
-    ctx.translate(x + _w, y + _h);
-    ctx.rotate(r * (Math.PI/180));
-
-    ctx.beginPath();
-    ctx.rect(-_w, -_h, w, h);
-    ctx.closePath();
-
-    ctx.stroke();
-    ctx.fillStyle = randRGB();
-    ctx.fill();
-    ctx.restore();
-
-    x += dx;
-    y += dy;
-    w += dw;
-    h += dh;
-    r += dr;
-
+$(function() {
+  var $select = $('#select');
+  for (var func in drawingFunctions) {
+    $select.append($('<option value="' + func + '">' + func + '</option>'));
   }
-
-}
-
-// var draw = rainbowRectangles;
-var draw = rotatingRectangles;
-
-
-window.onload = function() {
-  var canvas = document.getElementById('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  draw(); 
-}
+  $select.on('change', function(e) {
+    resetCanvas();
+    drawCurrentSelection();
+  }).trigger('change');
+});
