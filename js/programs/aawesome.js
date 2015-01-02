590ac36e56103709607d714d78243a31690fc28f
Andrew Boekhoff <andrew.boekhoff@kaybus.com>
2015-01-01 20:59:46 -0800

    add animation

--- js/programs/aawesome.js
+++ js/programs/aawesome.js
@@ -1,8 +1,43 @@
 App.programs.aawesome = function() {
 
+  var ctx = App.getContext();
+  var screenWidth = ctx.canvas.width;
+  var screenHeight = ctx.canvas.height;
+
+  // var x = Math.random() * screenWidth;
+  // var y = Math.random() * screenHeight;
+
+  var x = 10;
+  var y = 10;
+
+  var dx = 14;
+  var dy = 1;
+
+  function clearScreen() {
+    ctx.canvas.width = canvas.width;
+  }
+
+  function draw() {
+    clearScreen();
+    ctx.rect(x, y, 30, 30);
+    ctx.fillStyle = App.randRGB();
+    ctx.fill();
+  }
+
+  function update() {
+    x += dx;
+    y += dy;
+
+    if (x < 0) { dx = -dx; x = 0; }
+    if (x > screenWidth) { dx = -dx; x = screenWidth; }
+    if (y < 0) { dy = -dy; y = 0; }
+    if (y > screenHeight) { dy = -dy; y = screenHeight; }
+  }
+
   function animate(time) {
     window.requestAnimationFrame(animate);
-    console.log(time);
+    draw();
+    update();
   }
 
   animate();
